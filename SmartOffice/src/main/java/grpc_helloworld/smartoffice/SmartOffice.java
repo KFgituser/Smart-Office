2a0be38f23314801285d14d408199b2447ad1bb8
Eric gituser <137890466+KFgituser@users.noreply.github.com>
2025-04-10 14:34:34 +0100

    Add files via upload

---
+++ SmartOffice/src/main/java/grpc_helloworld/smartoffice/SmartOffice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grpc_helloworld.smartoffice;
+
+/**
+ *
+ * @author hkf
+ */
+public class SmartOffice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
